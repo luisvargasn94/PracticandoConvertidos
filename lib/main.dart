821af8db5baae5fb514e9690bf0545ec0fc89b5b
Carlos Sanchez <carlos.sanchez.a0@gmail.com>
2021-08-10 21:16:56 -0500

    ajustes de drawer

--- lib/main.dart
+++ lib/main.dart
@@ -18,36 +18,135 @@ class _MyAppState extends State<MyApp> {
     'Kilometros',
     'Pies',
     'Millas',
-    'Libras'
-        'Onzas'
+    'Libras',
+    'Onzas'
   ];
-  String _luiM = 'Centimetros';
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('Convertidor de Unidades'),
+    return MaterialApp(home: MyDrawer());
+  }
+}
+
+class MyDrawer extends StatefulWidget {
+  @override
+  _MyDrawerState createState() => _MyDrawerState();
+}
+
+class _MyDrawerState extends State<MyDrawer> {
+  @override
+  String _luiM = '';
+  Widget build(BuildContext context) {
+    String _miimage =
+        'https://images.unsplash.com/photo-1628613779039-7a71e2df81d5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=626&q=80';
+    return Scaffold(
+      appBar: AppBar(title: Text('Convertidor de Unidades')),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                image: DecorationImage(
+                    image: NetworkImage(_miimage), fit: BoxFit.cover),
+
+                // image: CircleAvatar(backgroundImage: NetworkImage(_miimage)),
+              ),
+              child: Text('mi hader'),
+            ),
+            ListTile(
+              title: const Text('Centimetros'),
+              onTap: () {
+                _mifuncion(context, 'centimetros');
+              },
+            ),
+            ListTile(
+              title: const Text('Metros'),
+              onTap: () {
+                _mifuncion(context, 'Metros');
+              },
+            ),
+            ListTile(
+              title: const Text('Kilometros'),
+              onTap: () {
+                _mifuncion(context, 'Kilometros');
+              },
+            ),
+            ListTile(
+              title: const Text('Pies'),
+              onTap: () {
+                _mifuncion(context, 'Pies');
+              },
+            ),
+            ListTile(
+              title: const Text('Millas'),
+              onTap: () {
+                _mifuncion(context, 'Millas');
+              },
+            ),
+            ListTile(
+              title: const Text('Libras'),
+              onTap: () {
+                _mifuncion(context, 'Libras');
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: const Text('Onzas'),
+              onTap: () {
+                _mifuncion(context, 'Onzas');
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ),
       ),
       body: Container(
         child: Column(children: [
+          Column(
+            children: [
+              Text(
+                _luiM.toString(),
+                style: TextStyle(
+                  color: Colors.blueAccent,
+                  fontSize: 30.0,
+                  backgroundColor: Colors.yellow,
+                ),
+              ),
+            ],
+          ),
           Text(_luiM),
           TextField(
             onChanged: (value) {
               print(value);
             },
           ),
-          DropdownButton<String>(
-              value: _luiM,
-              items: _medidas.map((m) {
-                return DropdownMenuItem(value: m, child: Text(m));
-              }).toList(),
-              onChanged: (value) {
-                print(_luiM);
-                setState(() {});
-              })
+
+          // DropdownButton<String>(
+          //     value: _luiM,
+          //     items: _medidas.map((m) {
+          //       return DropdownMenuItem(value: m, child: Text(m));
+          //     }).toList(),
+          //     onChanged: (value) {
+          //       print(_luiM);
+          //       setState(() {});
+          //     })
         ]),
       ),
-    ));
+    );
+  }
+
+  void _mifuncion(BuildContext context, String mistring) {
+    setState(() {
+      _luiM = mistring;
+    });
+    Navigator.pop(context);
   }
 }
